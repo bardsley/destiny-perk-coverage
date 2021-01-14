87b54c420dcce247d96f8b16eceddbdb597d4231
Adam Bardsley <adam.bardsley@gmail.com>
2021-01-14 13:03:36 +0000

    Got me a Token! -- Basic Auth now working, state not stored

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -4,6 +4,7 @@
 
 <script>
 import ClientOAuth2 from "client-oauth2";
+// import cryptoRandomString from "crypto-random-string"; 
 
 let bungieAuth = new ClientOAuth2({
   clientId: "35202",
@@ -11,7 +12,8 @@ let bungieAuth = new ClientOAuth2({
   accessTokenUri: "https://www.bungie.net/platform/app/oauth/token/",
   authorizationUri: "https://www.bungie.net/en/oauth/authorize",
   redirectUri: "https://destiny-perk-coverage.web.app/auth/bungie/callback",
-  state: "Ilugsbdkjasghd;lkahnsdo;ulh;oi'oj;kjd"
+  // state:  cryptoRandomString({ length: 10, type: "url-safe" }),
+  state: 'STATICSTATETEST'
 });
 
 // let returnedUri = 'https://localhost:8080/auth/bungie/callback?code=cd426fd756780f00ec5eec9f148c5637'
@@ -20,14 +22,15 @@ let bungieAuth = new ClientOAuth2({
 
 export default {
   props: ["authCode"],
-  created() {
+  async created() {
     if(this.authCode) {
       console.log("get a Access Token")
       console.log(this.$router.currentRoute.path)
       console.log(window.location.host)
       let thisUri = 'https://destiny-perk-coverage.web.app/auth/bungie/callback?code='+this.authCode
       thisUri = window.location.href
-      console.log(bungieAuth.code.getToken(thisUri,{ headers: { 'x-api-key': '1c57e1390a134c1492de01238975680e'} }))
+      let token = await bungieAuth.code.getToken(thisUri,{ headers: { 'x-api-key': '1c57e1390a134c1492de01238975680e'} })
+      console.log('TOKEN',token)
     } else {
       let requestUri = bungieAuth.code.getUri();
       console.log(requestUri);
