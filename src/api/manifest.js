1fb852c1a35821316da341bf8d707c5629ca2cf3
Adam Bardsley <adam.bardsley@gmail.com>
2021-01-17 14:54:58 +0000

    Basic listing per character

--- src/api/manifest.js
+++ src/api/manifest.js
@@ -3,8 +3,15 @@ import axios from 'axios'
 const getItemDefinition = async (hash) => {
     let request = await axios.get('https://destiny-perk-coverage-default-rtdb.europe-west1.firebasedatabase.app/manifest/inventoryItems/'+hash+'.json', {
     });
-    console.log(hash,request)
+    // console.log(hash,request)
     return request.data
 }
 
-export { getItemDefinition }
+const getCategoryDefinition = async (hash) => {
+    let request = await axios.get('https://destiny-perk-coverage-default-rtdb.europe-west1.firebasedatabase.app/manifest/itemCategories/'+hash+'.json', {
+    });
+    // console.log(hash,request)
+    return request.data
+}
+
+export { getItemDefinition,getCategoryDefinition}
--- src/views/CharacterSelect.vue
+++ src/views/CharacterSelect.vue
@@ -1,51 +1,31 @@
 <template>
   <div class="character-select">
     <h1>Select a character</h1>
-    <!-- <pre>
-            {{ characters }}emblemColor": {
-    "red": 0,
-    "green": 107,
-    "blue": 255,
-    "alpha": 255
-  }
-        </pre> -->
-    <div
-      v-for="(char, name) in characters"
-      v-bind:key="name"
-      :style="
-        'background-color: rgb(' +
-        char.emblemColor.red +
-        ',' +
-        char.emblemColor.green +
-        ',' +
-        char.emblemColor.blue +
-        ',0.2);'
-      "
-    >
-      <div
-        class="character"
-        :style="
-          'background-image: url(\'//bungie.net' +
-          char.emblemBackgroundPath +
-          '\');'
-        "
-      >
+    <div v-for="(char, name) in characters" v-bind:key="name"
+      :style="'background-color: rgb(' + char.emblemColor.red + ',' + char.emblemColor.green + ',' + char.emblemColor.blue + ',0.2);'">
+      <div class="character" :style="'background-image: url(\'//bungie.net' + char.emblemBackgroundPath + '\');'">
         <div class="contents">
           {{ char.light }}
         </div>
       </div>
-
-      <pre>
-                {{ char }}
-            </pre
-      >
+      <div class="inventory">
+        <div class="item" v-for="(item,key) in char.inventory" v-bind:key="key">
+          <img v-if="item.displayProperties.hasIcon" :src="'//bungie.net'+item.displayProperties.icon"/><br/>
+          <div class="details">
+            <h2>{{ item.displayProperties.name }}</h2>
+            <pre>{{ item.itemCategoryHashes }}</pre>
+          </div>
+        </div>
+      </div>
+      
+      <!-- <pre>{{ char }} </pre> -->
     </div>
 
-    <ul>
+    <!-- <ul>
       <li v-for="item in equipment" :key="item.itemHash">
-        {{ item }}
+        <pre>{{ item }}</pre>
       </li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
@@ -65,35 +45,48 @@ export default {
       membershipId: null,
       membershipType: null,
       characters: {},
-      equipment: [],
     };
   },
   async created() {
+    let vm = this
     this.membership = await getMembership();
     this.membershipId = this.membership.destinyMemberships[0].membershipId;
     this.membershipType = this.membership.destinyMemberships[0].membershipType;
     this.characters = await getCharacters(this.membershipType,this.membershipId)
-    let characterId = Object.keys(this.characters)[0]
-    let characterItems = await getCharacterItems(this.membershipType,this.membershipId,characterId)
-    console.debug(characterItems)
-    let tempEquipment = characterItems.equipment.data.items
-    console.log(tempEquipment)
-    let downloadedEquipment = await Promise.all(
-        tempEquipment.map(async (item) => {
-            console.log(item.itemHash)
-            let itemDef = await getItemDefinition(item.itemHash)
-            return itemDef
-        })
-    )
-    console.log(downloadedEquipment)
-    this.equipment = downloadedEquipment
+    Object.keys(this.characters).forEach( async (characterId) => { 
+      let inventory = await this.characterEquipment(characterId)
+      vm.$set(this.characters[characterId],'inventory',inventory)
+    })
   },
-  methods: {},
+  methods: {
+    async characterEquipment(characterId) {
+      let characterItems = await getCharacterItems(this.membershipType,this.membershipId,characterId)
+      let tempEquipment = characterItems.equipment.data.items
+      let downloadedEquipment = await Promise.all(
+        tempEquipment.map(async (item) => {
+          let itemDef = await getItemDefinition(item.itemHash)
+          return itemDef
+        })
+      )
+      console.log(downloadedEquipment)
+      return downloadedEquipment
+    },
+    async loadCategories() {
+      
+    }
+  },
 };
 
 </script>
 
 <style lang="scss" scoped>
+.character-select .inventory {
+  display:flex;
+  flex-wrap: wrap;
+}
+.character-select .inventory .item { width: 50%; display: flex;}
+.character-select .inventory .item img { flex-grow: 2; max-width:100px; max-height: 100px;}
+.character-select .inventory .item .details { flex-grow: 2; margin-left: 1em;}
 .character-select .character {
   width: 100%;
   padding-top: 20%;
