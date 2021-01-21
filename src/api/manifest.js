8d322424a5a8423be1a9db0bd8c2cdfb8bfb7de8
Adam Bardsley <adam.bardsley@gmail.com>
2021-01-21 12:55:49 +0000

    Slight additions

--- src/api/manifest.js
+++ src/api/manifest.js
@@ -1,8 +1,23 @@
 import axios from 'axios'
+import { tokenExpired, refreshToken } from './refreshToken'
+
 
 const getItemDefinition = async (hash) => {
     let request = await axios.get('https://destiny-perk-coverage-default-rtdb.europe-west1.firebasedatabase.app/manifest/inventoryItems/' + hash + '.json', {});
     // console.log(hash,request)
+    return request.data
+}
+
+const getItemInstance = async (memTypeId, memId, itemInstanceId) => {
+    if (tokenExpired()) { await refreshToken() }
+    let access_token = JSON.parse(localStorage.getItem('token')).access_token
+    let components = "ItemInstances,ItemPerks,ItemSockets,ItemReusablePlugs" //,ItemTalentGrids,ItemPlugStates,PresentationNodes"
+    let request = await axios.get(`https://www.bungie.net/platform/Destiny2/${memTypeId}/Profile/${memId}/Item/${itemInstanceId}/?components=${components}`,{
+        headers: {
+            Authorization: 'Bearer ' + access_token,
+            'x-api-key': localStorage.getItem('x-api-key'),
+        }
+    })
     return request.data
 }
 
@@ -30,4 +45,4 @@ const setUpCategories = async () => {
     }
   }
 
-export { getItemDefinition, getCategoryDefinition, getCategories, setUpCategories }
+export { getItemDefinition, getItemInstance, getCategoryDefinition, getCategories, setUpCategories }
--- src/views/CharacterSelect.vue
+++ src/views/CharacterSelect.vue
@@ -2,26 +2,32 @@
   <div class="character-select">
     <h1>Select a character</h1>
     <div v-for="(char, name) in characters" v-bind:key="name"
-      @click="char.inventory && char.showInventory ? char.showInventory = false : characterEquipment(char.characterId)"
       :style="'background-color: rgb(' + char.emblemColor.red + ',' + char.emblemColor.green + ',' + char.emblemColor.blue + ',0.2);'">
-      <div class="character" :style="'background-image: url(\'//bungie.net' + char.emblemBackgroundPath + '\');'">
+      <div class="character" :style="'background-image: url(\'//bungie.net' + char.emblemBackgroundPath + '\');'"
+        @click="char.inventory && char.showInventory ? char.showInventory = false : characterEquipment(char.characterId)">
         <div class="contents">
           <h2>{{ char.light }}</h2>
           <p>Last Played: {{ $dateStr(Date.parse(char.dateLastPlayed)) }}</p>
         </div>
       </div>
-      <div v-if="char.inventory && char.showInventory" class="inventory">
-        <h2>Equipped</h2>
-        <div class="item" v-for="(item,key) in char.inventory" v-bind:key="key">
-          <img v-if="item.displayProperties.hasIcon" :src="'//bungie.net'+item.displayProperties.icon"/><br/>
-          <div class="details">
-            <h2>{{ item.displayProperties.name }}</h2>
-            <span v-for="(hashId) in item.itemCategoryHashes" :key="hashId">
-              {{categories[hashId].displayProperties.name || "n/a"}},
-            </span>
-            
-          </div>
+      <div v-if="char.showInventory" class="inventory">
+        <div v-if="!char.inventory">
+          Loading
         </div>
+        <template v-else>
+          <h2>Equipped</h2>
+          <div class="item" v-for="(item,key) in char.inventory" v-bind:key="key"
+            @click="inspectItem(item.itemInstanceId)" >
+            <img v-if="item.displayProperties.hasIcon" :src="'//bungie.net'+item.displayProperties.icon"/><br/>
+            <div class="details">
+              <h2>{{ item.displayProperties.name }}</h2>
+              <span v-for="(hashId) in item.itemCategoryHashes" :key="hashId">
+                {{categories[hashId].displayProperties.name || "n/a"}},
+              </span>
+              
+            </div>
+          </div>
+        </template>
       </div>
       
       <!-- <pre>{{ char }} </pre> -->
@@ -42,7 +48,7 @@ import {
   getMembership,
   getCharacterItems,
 } from "@/api/getCharacters";
-import { getItemDefinition,setUpCategories } from "@/api/manifest";
+import { getItemDefinition,setUpCategories,getItemInstance } from "@/api/manifest";
 
 export default {
   data() {
@@ -61,10 +67,6 @@ export default {
     this.membershipId = this.membership.destinyMemberships[0].membershipId;
     this.membershipType = this.membership.destinyMemberships[0].membershipType;
     this.characters = await getCharacters(this.membershipType,this.membershipId)
-    // Object.keys(this.characters).forEach( async (characterId) => { 
-    //   let inventory = await this.characterEquipment(characterId)
-    //   vm.$set(this.characters[characterId],'inventory',inventory)
-    // })
   },
   methods: {
     async characterEquipment(characterId) {
@@ -76,14 +78,17 @@ export default {
       let downloadedEquipment = await Promise.all(
         tempEquipment.map(async (item) => {
           let itemDef = await getItemDefinition(item.itemHash)
-          return itemDef
+          return Object.assign(itemDef,item)
         })
       )
-      // console.log(downloadedEquipment)
-      // return downloadedEquipment
       this.$set(this.characters[characterId],'inventory',downloadedEquipment)
     },
-    
+    async inspectItem(itemInstanceId) {
+      let itemInfo = getItemInstance(this.membershipType,this.membershipId,itemInstanceId).then((itemInfo) => {
+        console.log(itemInfo)
+      })
+      return itemInfo
+    }
   },
 };
 
